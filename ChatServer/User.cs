d3b9cb4050987319efbfea0c26654565ad000543
Programir <zzradio@mail.ru>
2019-03-17 19:46:34 +0200

    Bug fixes. Created User class.

---
+++ ChatServer/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    class User
+    {
+        string UserName { get; set; }
+        string Password { get; set; }
+        DateTime DatOfBirth { get; set; }
+        string Email { get; set; }
+
+        public User(string UserName, string Password, DateTime DatOfBirth, string Email)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+            this.DatOfBirth = DatOfBirth;
+            this.Email = Email;
+        }
+    }
+}
